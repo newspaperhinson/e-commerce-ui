17f42f2d6e9eef988c60ec44cd9bbe99c74a6ab5
Hinson <hinson.po@zlashy.com>
2025-04-21 01:45:49 +0800

    feat: add header component with logo and shopping cart icon

---
+++ src/lib/components/organisms/Header..svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { ShoppingCart } from '@lucide/svelte';
+
+	import Logo from '$lib/components/atoms/Logo.svelte';
+</script>
+
+<header
+	class="bg-primary-950 text-primary-50 flex items-center justify-between px-8 py-4 shadow-md"
+>
+	<a href="/" class="flex items-center gap-2">
+		<Logo class="h-8 w-8" fill="currentColor" />
+		<p class="ml-2 text-xl font-bold">LogicMart</p>
+	</a>
+	<div class="relative cursor-pointer">
+		<span class="badge-icon preset-filled-primary-500 absolute -top-4 -right-4 z-10 h-2 w-2">3</span
+		>
+		<ShoppingCart />
+	</div>
+</header>

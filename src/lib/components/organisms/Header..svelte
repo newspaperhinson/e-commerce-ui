0c190a47ee463c47a677e0e0ff5162966ca9c0c4
Hinson <hinson.po@zlashy.com>
2025-04-21 02:28:34 +0800

    Change header colours to surface

--- src/lib/components/organisms/Header..svelte
+++ src/lib/components/organisms/Header..svelte
@@ -5,14 +5,14 @@
 </script>
 
 <header
-	class="bg-primary-950 text-primary-50 flex items-center justify-between px-8 py-4 shadow-md"
+	class="bg-surface-950 text-surface-50 flex items-center justify-between px-8 py-4 shadow-md"
 >
 	<a href="/" class="flex items-center gap-2">
 		<Logo class="h-8 w-8" fill="currentColor" />
 		<p class="ml-2 text-xl font-bold">LogicMart</p>
 	</a>
 	<div class="relative cursor-pointer">
-		<span class="badge-icon preset-filled-primary-500 absolute -top-4 -right-4 z-10 h-2 w-2">3</span
+		<span class="badge-icon preset-filled-surface-500 absolute -top-4 -right-4 z-10 h-2 w-2">3</span
 		>
 		<ShoppingCart />
 	</div>
